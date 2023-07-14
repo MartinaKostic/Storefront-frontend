ceaef5708d9c0f72350a58ee8c758d4946f293d3
MartinaKostic <martina.kostic@gmail.com>
2023-07-14 19:47:31 +0200

    App.js changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import { MedusaProvider } from "medusa-react";
-import Products from "./components/Products/Products";
-import Cart from "./components/Cart/Cart";
+import { CartProvider, MedusaProvider } from "medusa-react";
 import { QueryClient } from "@tanstack/react-query";
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Products from "./components/Products/Products";
+import Cart from "./components/Cart/Cart";
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
+import ProductDetails from "./components/ProductDetails/ProductDetails";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div>
-      <Home></Home>
+    <div className="min-h-screen bg-background">
       <MedusaProvider
         queryClientProviderProps={{ client: queryClient }}
         baseUrl="http://localhost:9000"
       >
-        <Products />
+        <CartProvider>
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/productdetails/:id" element={<ProductDetails />} />
+            </Routes>
+          </Router>
+        </CartProvider>
       </MedusaProvider>
     </div>
   );
