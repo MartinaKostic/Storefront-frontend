d692e60f6092184e966b15cf4dbd3d1f21677e72
MartinaKostic <martina.kostic@gmail.com>
2023-07-14 19:46:30 +0200

    Home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,17 @@
-function Home() {
+import picture from "../../pics/hero-merch.png";
+const Home = () => {
   return (
-    <div>
-      <h1>Store</h1>
-      <h2>Check out our products!</h2>
-      <h2>Contact us!</h2>
+    <div className="text-center p-10 flex flex-col">
+      <h1 className="text-5xl text-primary_varient font-medium">
+        Welcome to our Medusa Store!
+      </h1>
+      <img className="h-64 self-center" src={picture} alt="picture"></img>
+      <p className="text-md">
+        You can find all of the Medusa merch by clicking on Products button.
+      </p>
+      <p className="text-2xl">Happy shopping!</p>
     </div>
   );
-}
+};
 
 export default Home;
